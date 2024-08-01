5c90605a0d5fe1e83f72442cb61e66d5b106bd1e
Victor Kabata <victorbro14@gmail.com>
2024-08-01 20:36:15 +0300

    Configured android gradle file

--- app-android/build.gradle.kts
+++ app-android/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.compose.compiler)
 }
 
@@ -38,9 +38,10 @@ android {
 
 dependencies {
     implementation(projects.shared)
-    implementation(libs.compose.ui)
-    implementation(libs.compose.ui.tooling.preview)
-    implementation(libs.compose.material3)
-    implementation(libs.androidx.activity.compose)
-    debugImplementation(libs.compose.ui.tooling)
+
+    implementation(libs.compose.activity)
+    implementation(libs.appCompat)
+    implementation(libs.koin.android)
+
+    // debugImplementation(libs.compose.ui.tooling)
 }
