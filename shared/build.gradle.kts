277e9e1853b3467387a7e40de0df1feed184cc4b
Victor Kabata <victorbro14@gmail.com>
2024-08-02 00:08:13 +0300

    Set up sqlDelight database factory

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -83,8 +83,8 @@ android {
         minSdk = 24
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
