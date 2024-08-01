90827ca5fd5aec1a4cb64c56678648121843722a
Victor Kabata <victorbro14@gmail.com>
2024-08-01 23:31:34 +0300

    Create Constants.kt

---
+++ shared/src/commonMain/kotlin/com/vickbt/carrystore/domain/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.vickbt.carrystore.domain.utils
+
+object Constants {
+
+    const val BASE_URL = "my-json-server.typicode.com"
+    const val URL_PATH = "carry1stdeveloper/mock-product-api/"
+
+}
