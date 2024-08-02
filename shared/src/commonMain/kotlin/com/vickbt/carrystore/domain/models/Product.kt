aa41eb1592ba055db3a6c2382550995df9ab7605
Victor Kabata <victorbro14@gmail.com>
2024-08-02 18:24:35 +0300

    Create Product.kt

---
+++ shared/src/commonMain/kotlin/com/vickbt/carrystore/domain/models/Product.kt
@@ -0,0 +1,13 @@
+package com.vickbt.carrystore.domain.models
+
+data class Product(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val price: Int,
+    val currencyCode: String,
+    val currencySymbol: String,
+    val quantity: Int,
+    val imageLocation: String,
+    val status: String
+)
