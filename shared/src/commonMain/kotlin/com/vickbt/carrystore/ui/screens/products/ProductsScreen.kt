09e676459e9c5ffaa97e96cab5c086297da43cf6
Victor Kabata <victorbro14@gmail.com>
2024-08-03 10:41:31 +0300

    Set up product screen

--- shared/src/commonMain/kotlin/com/vickbt/carrystore/ui/screens/products/ProductsScreen.kt
+++ shared/src/commonMain/kotlin/com/vickbt/carrystore/ui/screens/products/ProductsScreen.kt
@@ -1,14 +1,23 @@
-@file:OptIn(KoinExperimentalAPI::class)
+@file:OptIn(KoinExperimentalAPI::class, ExperimentalLayoutApi::class)
 
 package com.vickbt.carrystore.ui.screens.products
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.vickbt.carrystore.ui.components.ItemProduct
+import com.vickbt.carrystore.ui.navigation.NavigationItem
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.annotation.KoinExperimentalAPI
 
@@ -18,13 +27,30 @@ fun ProductsScreen(
     viewModel: ProductsViewModel = koinViewModel<ProductsViewModel>()
 ) {
 
+    println("Victor, ProductsScreen")
     val productsUiState = viewModel.products.collectAsState().value
 
-    println("Victor ProductsScreen")
-    println("Victor Products: ${productsUiState.products}")
-    println("Victor Is loading: ${productsUiState.isLoading}")
-    println("Victor Error: ${productsUiState.errorMessage}")
+    println("Victor Products in UI: ${productsUiState.products}")
 
-    Box(modifier = Modifier.background(Color.Red))
-
+    Box(modifier = Modifier.fillMaxSize()) {
+        if (productsUiState.isLoading) {
+            // ToDo: Show shimmer
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        } else if (!productsUiState.errorMessage.isNullOrEmpty()) {
+            // ToDo: Show error message
+        } else {
+            LazyVerticalGrid(
+                modifier = Modifier.fillMaxSize().align(Alignment.Center),
+                columns = GridCells.Fixed(2),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally)
+            ) {
+                items(productsUiState.products ?: emptyList()) { product ->
+                    ItemProduct(modifier = Modifier, product = product) {
+                        navHostController.navigate(NavigationItem.Products.route)
+                    }
+                }
+            }
+        }
+    }
 }
