8749e58e04a15141da998dd45716df28bfb53324
Victor Kabata <victorbro14@gmail.com>
2024-08-03 15:51:03 +0300

    Completed details screen UI

--- shared/src/commonMain/kotlin/com/vickbt/carrystore/ui/screens/product_details/ProductDetailsScreen.kt
+++ shared/src/commonMain/kotlin/com/vickbt/carrystore/ui/screens/product_details/ProductDetailsScreen.kt
@@ -2,26 +2,38 @@
 
 package com.vickbt.carrystore.ui.screens.product_details
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import coil3.compose.AsyncImage
-import com.vickbt.carrystore.domain.models.Product
 import com.vickbt.carrystore.ui.screens.products.ProductsViewModel
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.annotation.KoinExperimentalAPI
@@ -34,27 +46,110 @@ fun ProductDetailsScreen(
     viewModel: ProductsViewModel = koinViewModel<ProductsViewModel>()
 ) {
 
-    LazyColumn(modifier = Modifier.fillMaxSize().padding(paddingValues)) {
-        item {
-            Box(modifier = Modifier.fillMaxWidth().fillMaxHeight(.50f)) {
-                AsyncImage(
-                    modifier = Modifier.fillMaxSize(),
-                    model = "https://dev-images-carry1st-products.s3.eu-west-2.amazonaws.com/a3fc8953-ba5b-4fb4-9017-223557716594.png",
+    val columnScrollState = rememberScrollState()
+
+    Column(
+        modifier = Modifier.fillMaxSize().verticalScroll(columnScrollState).padding(paddingValues),
+    ) {
+        Box(modifier = Modifier.fillMaxWidth().fillMaxHeight(.50f)) {
+            AsyncImage(
+                modifier = Modifier.fillMaxSize(),
+                model = "https://dev-images-carry1st-products.s3.eu-west-2.amazonaws.com/a3fc8953-ba5b-4fb4-9017-223557716594.png",
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                alignment = Alignment.Center,
+            )
+
+            IconButton(
+                modifier = Modifier.align(Alignment.TopStart).padding(16.dp),
+                onClick = { navHostController.navigateUp() }) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Rounded.ArrowBack,
                     contentDescription = null,
-                    contentScale = ContentScale.Crop,
-                    alignment = Alignment.Center,
+                    tint = MaterialTheme.colorScheme.onSurface
                 )
+            }
+        }
 
-                IconButton(
-                    modifier = Modifier.align(Alignment.TopStart).padding(16.dp),
-                    onClick = { navHostController.navigateUp() }) {
-                    Icon(
-                        imageVector = Icons.AutoMirrored.Rounded.ArrowBack,
-                        contentDescription = null,
-                        tint = MaterialTheme.colorScheme.onSurface
-                    )
-                }
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp, vertical = 8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "Product Name",
+                fontSize = 24.sp,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                fontWeight = FontWeight.SemiBold,
+                color = MaterialTheme.colorScheme.onSurface
+            )
 
+            Text(
+                text = "USD 1",
+                fontSize = 24.sp,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+        }
+
+        Text(
+            modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+            text = "Description",
+            fontSize = 16.sp,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.onSurface
+        )
+
+        Spacer(modifier = Modifier.height(2.dp))
+
+        Text(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            text = "Description Content",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Medium,
+            color = MaterialTheme.colorScheme.onSurface.copy(alpha = .85f)
+        )
+
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(4.dp, Alignment.CenterVertically)
+        ) {
+            Button(
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp, vertical = 8.dp),
+                onClick = {},
+                shape = MaterialTheme.shapes.medium
+            ) {
+                Text(
+                    modifier = Modifier.padding(vertical = 6.dp),
+                    text = "Add to Cart",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+            }
+
+            Button(
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp, vertical = 8.dp),
+                onClick = {},
+                shape = MaterialTheme.shapes.medium,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.surface,
+                    contentColor = MaterialTheme.colorScheme.onSurface
+                ),
+                border = BorderStroke(2.dp, MaterialTheme.colorScheme.primary)
+            ) {
+                Text(
+                    modifier = Modifier.padding(vertical = 6.dp),
+                    text = "Buy Now",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer
+                )
             }
         }
     }
