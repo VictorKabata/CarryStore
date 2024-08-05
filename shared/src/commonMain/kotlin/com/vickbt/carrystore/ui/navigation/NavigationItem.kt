0215ec1128ebd1431f08e8e965011213a4d6af3b
Victor Kabata <victorbro14@gmail.com>
2024-08-05 21:30:46 +0300

    Update NavigationItem.kt

--- shared/src/commonMain/kotlin/com/vickbt/carrystore/ui/navigation/NavigationItem.kt
+++ shared/src/commonMain/kotlin/com/vickbt/carrystore/ui/navigation/NavigationItem.kt
@@ -7,6 +7,5 @@ import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class NavigationItem(val route: String, val title: String, val icon: ImageVector) {
     data object Products : NavigationItem("products", "Products", Icons.Rounded.Home)
-
     data object Cart : NavigationItem("cart", "Cart", Icons.Rounded.ShoppingCart)
 }
