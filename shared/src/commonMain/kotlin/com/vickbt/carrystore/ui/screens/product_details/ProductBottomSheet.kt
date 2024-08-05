57c23943f4780e3aa8d01828f03a0efffe8e137d
Victor Kabata <victorbro14@gmail.com>
2024-08-05 03:18:26 +0300

    Update product detail item

--- shared/src/commonMain/kotlin/com/vickbt/carrystore/ui/screens/product_details/ProductBottomSheet.kt
+++ shared/src/commonMain/kotlin/com/vickbt/carrystore/ui/screens/product_details/ProductBottomSheet.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -19,7 +18,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SheetState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -37,101 +35,108 @@ fun ProductBottomSheet(
     modifier: Modifier = Modifier,
     product: Product,
     onAddToCartClicked: (Product) -> Unit,
-    onBuyNowClicked: (Product) -> Unit,
-    onDismiss: () -> Unit
+    onBuyNowClicked: (Product) -> Unit
 ) {
 
     val columnScrollState = rememberScrollState()
 
-    Column(modifier = modifier.fillMaxSize().verticalScroll(columnScrollState)) {
-        AsyncImage(
-            modifier = Modifier.fillMaxWidth().fillMaxHeight(.40f),
-            model = product.imageLocation,
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            alignment = Alignment.Center,
-        )
-
-        Row(
-            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp, vertical = 8.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(
-                text = product.name,
-                fontSize = 24.sp,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                fontWeight = FontWeight.SemiBold,
-                color = MaterialTheme.colorScheme.onSurface
-            )
-
-            Text(
-                text = "${product.currencyCode} ${product.price}",
-                fontSize = 24.sp,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                fontWeight = FontWeight.Bold,
-                color = MaterialTheme.colorScheme.onSurface
-            )
-        }
-
-        Text(
-            modifier = Modifier.padding(horizontal = 16.dp, vertical = 4.dp),
-            text = "Description",
-            fontSize = 16.sp,
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis,
-            fontWeight = FontWeight.Bold,
-            color = MaterialTheme.colorScheme.onSurface
-        )
-
-        Text(
-            modifier = Modifier.padding(horizontal = 16.dp),
-            text = product.description,
-            fontSize = 16.sp,
-            fontWeight = FontWeight.Medium,
-            color = MaterialTheme.colorScheme.onSurface.copy(alpha = .85f)
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
+    Box(modifier = modifier) {
         Column(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(4.dp, Alignment.CenterVertically)
+            modifier = Modifier.verticalScroll(columnScrollState),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Button(
+            AsyncImage(
+                modifier = Modifier.fillMaxSize(.40f),
+                model = product.imageLocation,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                alignment = Alignment.Center,
+            )
+
+            Row(
                 modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp, vertical = 8.dp),
-                onClick = { onAddToCartClicked(product) },
-                shape = MaterialTheme.shapes.medium
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 Text(
-                    modifier = Modifier.padding(vertical = 6.dp),
-                    text = "Add to Cart",
-                    fontSize = 16.sp,
+                    text = product.name,
+                    fontSize = 24.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
                     fontWeight = FontWeight.SemiBold,
-                    color = MaterialTheme.colorScheme.onPrimaryContainer
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+
+                Text(
+                    text = "${product.currencyCode} ${product.price}",
+                    fontSize = 24.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.onSurface
                 )
             }
 
-            Button(
-                modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp, vertical = 8.dp),
-                onClick = { onBuyNowClicked(product) },
-                shape = MaterialTheme.shapes.medium,
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = MaterialTheme.colorScheme.surface,
-                    contentColor = MaterialTheme.colorScheme.onSurface
-                ),
-                border = BorderStroke(2.dp, MaterialTheme.colorScheme.primary)
-            ) {
+            Column(modifier = Modifier.fillMaxWidth()) {
                 Text(
-                    modifier = Modifier.padding(vertical = 6.dp),
-                    text = "Buy Now",
+                    modifier = Modifier.padding(horizontal = 16.dp, vertical = 4.dp),
+                    text = "Description",
                     fontSize = 16.sp,
-                    fontWeight = FontWeight.SemiBold,
-                    color = MaterialTheme.colorScheme.onPrimaryContainer
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+
+                Text(
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                    text = product.description,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Medium,
+                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = .85f)
                 )
             }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(4.dp, Alignment.CenterVertically)
+            ) {
+                Button(
+                    modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp, vertical = 8.dp),
+                    onClick = { onAddToCartClicked(product) },
+                    shape = MaterialTheme.shapes.medium
+                ) {
+                    Text(
+                        modifier = Modifier.padding(vertical = 6.dp),
+                        text = "Add to Cart",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        color = MaterialTheme.colorScheme.onPrimaryContainer
+                    )
+                }
+
+                Button(
+                    modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp, vertical = 8.dp),
+                    onClick = { onBuyNowClicked(product) },
+                    shape = MaterialTheme.shapes.medium,
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.surface,
+                        contentColor = MaterialTheme.colorScheme.onSurface
+                    ),
+                    border = BorderStroke(2.dp, MaterialTheme.colorScheme.primary)
+                ) {
+                    Text(
+                        modifier = Modifier.padding(vertical = 6.dp),
+                        text = "Buy Now",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        color = MaterialTheme.colorScheme.onPrimaryContainer
+                    )
+                }
+            }
         }
+
     }
 }
--- shared/src/commonMain/kotlin/com/vickbt/carrystore/ui/screens/products/ProductsScreen.kt
+++ shared/src/commonMain/kotlin/com/vickbt/carrystore/ui/screens/products/ProductsScreen.kt
@@ -1,5 +1,5 @@
 @file:OptIn(
-    KoinExperimentalAPI::class, ExperimentalLayoutApi::class,
+    KoinExperimentalAPI::class,
     ExperimentalMaterial3Api::class
 )
 
@@ -7,11 +7,10 @@ package com.vickbt.carrystore.ui.screens.products
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -22,6 +21,7 @@ import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SheetValue
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.material3.rememberStandardBottomSheetState
@@ -34,6 +34,9 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.vickbt.carrystore.domain.models.Product
@@ -53,6 +56,9 @@ fun ProductsScreen(
 
     val productsUiState = viewModel.products.collectAsState().value
 
+    val localDensity = LocalDensity.current
+    var sheetContentHeight by remember { mutableStateOf(Dp.Unspecified) }
+
     var selectedProduct by remember { mutableStateOf<Product?>(null) }
 
     val sheetState = rememberStandardBottomSheetState(
@@ -63,52 +69,64 @@ fun ProductsScreen(
 
     val scope = rememberCoroutineScope()
 
-    BottomSheetScaffold(
-        modifier = Modifier.padding(paddingValues),
-        scaffoldState = bottomSheetScaffoldState,
-        sheetShape = MaterialTheme.shapes.medium,
-        sheetContainerColor = MaterialTheme.colorScheme.surface,
-        sheetContent = {
-            selectedProduct?.let {
-                ProductBottomSheet(
-                    modifier = Modifier.fillMaxWidth().fillMaxHeight(.60f),
-                    product = it,
-                    onAddToCartClicked = {
-                        viewModel.saveProduct(product = it)
-                        scope.launch { sheetState.hide() }
-                    },
-                    onBuyNowClicked = {
-                        scope.launch { sheetState.hide() }
-                    },
-                    onDismiss = { }
-                )
-            }
-        }) {
-        Box(modifier = Modifier.fillMaxSize()) {
-            if (productsUiState.isLoading) {
-                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
-            } else if (!productsUiState.errorMessage.isNullOrEmpty()) {
-                ErrorState(
-                    modifier = Modifier.align(Alignment.Center),
-                    errorIcon = Icons.Rounded.Person,
-                    errorMessage = productsUiState.errorMessage,
-                    actionMessage = "Reload"
-                )
-            } else {
-                LazyVerticalGrid(
-                    modifier = Modifier.fillMaxSize().align(Alignment.Center),
-                    columns = GridCells.Fixed(2),
-                    verticalArrangement = Arrangement.spacedBy(8.dp),
-                    horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally)
-                ) {
-                    items(productsUiState.products ?: emptyList()) { product ->
-                        ItemProduct(modifier = Modifier, product = product) {
-                            selectedProduct = product
-                            scope.launch {
-                                if (!sheetState.isVisible) {
-                                    sheetState.expand()
-                                } else {
-                                    sheetState.hide()
+    println("Victor Height: $sheetContentHeight")
+
+    Scaffold(modifier = Modifier.padding(paddingValues)){
+        BottomSheetScaffold(
+            // modifier = Modifier.padding(paddingValues),
+            scaffoldState = bottomSheetScaffoldState,
+            sheetShape = MaterialTheme.shapes.medium,
+            sheetContainerColor = MaterialTheme.colorScheme.surface,
+            sheetContent = {
+                selectedProduct?.let {
+                    ProductBottomSheet(
+                        modifier = Modifier.fillMaxWidth().height(sheetContentHeight)
+                            .onGloballyPositioned { coordinates ->
+                                sheetContentHeight = with(localDensity) {
+                                    if (sheetState.currentValue == SheetValue.Expanded) {
+                                        coordinates.size.height.toDp()
+                                    } else {
+                                        Dp.Unspecified
+                                    }
+                                }
+                            },
+                        product = it,
+                        onAddToCartClicked = {
+                            viewModel.saveProduct(product = it)
+                            scope.launch { sheetState.hide() }
+                        },
+                        onBuyNowClicked = {
+                            scope.launch { sheetState.hide() }
+                        }
+                    )
+                }
+            }) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                if (productsUiState.isLoading) {
+                    CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                } else if (!productsUiState.errorMessage.isNullOrEmpty()) {
+                    ErrorState(
+                        modifier = Modifier.align(Alignment.Center),
+                        errorIcon = Icons.Rounded.Person,
+                        errorMessage = productsUiState.errorMessage,
+                        actionMessage = "Reload"
+                    )
+                } else {
+                    LazyVerticalGrid(
+                        modifier = Modifier.fillMaxSize().align(Alignment.Center),
+                        columns = GridCells.Fixed(2),
+                        verticalArrangement = Arrangement.spacedBy(8.dp),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally)
+                    ) {
+                        items(productsUiState.products ?: emptyList()) { product ->
+                            ItemProduct(modifier = Modifier, product = product) {
+                                selectedProduct = product
+                                scope.launch {
+                                    if (!sheetState.isVisible) {
+                                        sheetState.expand()
+                                    } else {
+                                        sheetState.hide()
+                                    }
                                 }
                             }
                         }
