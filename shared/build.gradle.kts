98352c232bac184fcc17d08ad3d546865bbb2006
Victor Kabata <victorbro14@gmail.com>
2024-08-08 10:28:04 +0300

    Set up product details screen test

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -25,14 +25,12 @@ kotlin {
             jvmTarget.set(JvmTarget.JVM_11)
         }
     }
-
-    val iosTarget: (String, KotlinNativeTarget.() -> Unit) -> KotlinNativeTarget =
-        when {
-            System.getenv("SDK_NAME")?.startsWith("iphoneos") == true -> ::iosArm64
-            System.getenv("NATIVE_ARCH")?.startsWith("arm") == true -> ::iosSimulatorArm64
-            else -> ::iosX64
-        }
-    iosTarget("ios") {}
+    
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    )
 
     cocoapods {
         summary = "Some description for the Shared Module"
---
+++ shared/src/commonTest/kotlin/com/vickbt/carrystore/ui/details/ProductDetailBottomSheetTest.kt
@@ -0,0 +1,47 @@
+@file:OptIn(ExperimentalTestApi::class, ExperimentalTestApi::class)
+
+package com.vickbt.carrystore.ui.details
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsEnabled
+import androidx.compose.ui.test.assertIsNotEnabled
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.runComposeUiTest
+import com.vickbt.carrystore.ui.screens.details.ProductBottomSheet
+import com.vickbt.carrystore.utils.ProductHelper
+import kotlin.test.Test
+
+class ProductDetailBottomSheetTest {
+
+    @Test
+    fun `test_initial_state`() = runComposeUiTest {
+        val product = ProductHelper.product
+        var itemCount by mutableStateOf(1)
+
+        setContent {
+            ProductBottomSheet(
+                product = product,
+                itemCount = itemCount,
+                onItemCountChanged = { itemCount = it },
+                onAddToCartClicked = {},
+                onBuyNowClicked = {},
+                onIncrement = { itemCount +1 },
+                onDecrement = { itemCount -1 }
+            )
+        }
+
+        onNodeWithTag("screen_details").assertIsDisplayed()
+        onNodeWithTag("text_product_name").assertTextEquals(product.name)
+        onNodeWithTag("text_product_price").assertTextEquals("${product.currencyCode} ${product.price}")
+        onNodeWithTag("text_product_description").assertTextEquals(product.description)
+        onNodeWithTag("text_count").assertTextEquals(itemCount.toString())
+    }
+
+}
